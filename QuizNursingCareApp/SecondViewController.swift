2712041ce1eeb9f9c630a72bc7707c1a91249359
gokken <bsk_daisuki_626@ezweb.ne.jp>
2016-03-07 15:29:19 +0900

    Initial Commit

---
+++ QuizNursingCareApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QuizNursingCareApp
+//
+//  Created by 後藤健佑 on 2016/03/07.
+//  Copyright © 2016年 後藤健佑. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
